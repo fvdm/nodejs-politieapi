01cee6c2c2ec2d49494417be3bc58b3795aef468
Franklin <fr@nkl.in>
2021-10-27 12:13:04 +0200

    Refactor(_talk): Shorter User-Agent header

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ module.exports = class PolitieAPI {
       parameters,
       headers: {
         'Accept': 'application/json',
-        'User-Agent': 'https://github.com/fvdm/nodejs-politieapi',
+        'User-Agent': 'nodejs-politieapi',
       },
     };
 
